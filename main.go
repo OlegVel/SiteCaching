edc6b7fcfdfdebd760daeaa64eac5ebc5112b788
OlegVel <olegvelig@gmail.com>
2019-05-26 22:24:22 +0300

    Adding handler

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
+	http.Handle("/", SiteHandler())
 	fmt.Println("Hello!")
+	if err := http.ListenAndServe(":1080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func SiteHandler() http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Body: ", request.Body)
+	}
 }
